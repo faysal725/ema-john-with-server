7a5a10b1d2477bd5da42cfa223d810ccd38a40cf
faysal725 <afaysal725@gmail.com>
2021-06-13 12:06:53 +0600

    server added

---
+++ src/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDA60-QDVCBvt5D6m0iJZTOJ_Qk22AcvRk",
+  authDomain: "ema-john-simple-e257f.firebaseapp.com",
+  projectId: "ema-john-simple-e257f",
+  storageBucket: "ema-john-simple-e257f.appspot.com",
+  messagingSenderId: "305623615969",
+  appId: "1:305623615969:web:93234523a81edeba0aeaa0"
+};
+
+export default firebaseConfig;
